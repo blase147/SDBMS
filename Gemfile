0e1146afcf4f68b14fa43ecaaa7f17e18084d8d1
blase147 <solarmails2@gmail.com>
2023-08-31 11:03:05 +0100

    add nav

--- Gemfile
+++ Gemfile
@@ -11,6 +11,8 @@ gem 'devise'
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem "sprockets-rails"
 
+gem 'jquery-rails'
+
 # Use postgresql as the database for Active Record
 gem "pg", "~> 1.1"
 
---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,18 @@
+// app/assets/javascripts/application.js
+//= require jquery
+//= require jquery_ujs
+//= require_tree .
+
+
+
+// app/assets/javascripts/application.js
+$(document).ready(function() {
+    $('.menu-parent a').click(function(event) {
+      event.preventDefault();
+      $(this).parent().toggleClass('active');
+    });
+  });
+  
+  
+  
+  
