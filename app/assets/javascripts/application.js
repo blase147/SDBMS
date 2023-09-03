ce37be3428cade07cf74033281a10b035d506a6a
blase147 <solarmails2@gmail.com>
2023-09-04 00:00:28 +0100

    update dashboard

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,21 +7,22 @@
 
 // app/assets/javascripts/application.js
 $(document).ready(function() {
-  $('.menu-parent a ').click(function() {
+  $('.menu-parent ').click(function() {
     $(this).parent().toggleClass('active');
   });
 });
 
 
 
-  document.addEventListener("DOMContentLoaded", function() {
-    const userDiv = document.querySelector(".user");
-    const logoutDiv = document.querySelector("#logout");
+document.addEventListener("DOMContentLoaded", function() {
+  const userDiv = document.querySelector(".user");
+  const logoutDiv = document.querySelector("#logout");
 
-    userDiv.addEventListener("click", function() {
-      logoutDiv.style.display = logoutDiv.style.display === "none" ? "block" : "none";
-    });
+  userDiv.addEventListener("click", function() {
+    logoutDiv.classList.toggle("hidden");
   });
+});
+
 
 
   
