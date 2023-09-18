e801cb6addc3b4344b1cfaa00de9bff8f56cd201
blase147 <solarmails2@gmail.com>
2023-09-18 14:56:57 +0100

    update manage schools

--- app/models/manage_school.rb
+++ app/models/manage_school.rb
@@ -1,2 +1,15 @@
 class ManageSchool < ApplicationRecord
-end
+    # Validations
+    validates :school_name, presence: true, length: { maximum: 255 }
+    validates :school_phone_number, presence: true, format: { with: /\A\d{10}\z/, message: "must be a 11-digit number" }
+    validates :school_address, presence: true, length: { maximum: 255 }
+    validates :school_motto, presence: true, length: { maximum: 255 }
+    validates :school_vision, presence: true, length: { maximum: 255 }
+    validates :school_mission, presence: true, length: { maximum: 255 }
+    validates :school_logo, presence: true
+    validates :school_photo, presence: true
+    validates :hos_signature, presence: true
+
+    # Other model associations or methods can be defined here.
+  end
+  
